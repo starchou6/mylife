590eeb88d988d5f6cb269714585bd1f509c08b47
starchou6 <starchou666@gmail.com>
2023-06-01 20:28:59 +0900

    front init

--- src/App.js
+++ src/App.js
@@ -1,13 +1,52 @@
-import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons';
+import { AppstoreOutlined, PlusSquareOutlined } from '@ant-design/icons';
 import { Menu } from 'antd';
 import { useState } from 'react';
-import Minter from './components/Minter.js'
+import Minter from './components/Minter.js';
+import NFTList from './components/NFTList.js';
+
+const allNFTData = [
+  {
+      title: 'AllNFT 1',
+      description: 'AllNFT description 1',
+  },
+  {
+      title: 'AllNFT 2',
+      description: 'AllNFT description 2',
+  },
+  {
+      title: 'AllNFT 3',
+      description: ' AllNFTdescription 3',
+  },
+  {
+      title: 'AllNFT 4',
+      description: 'AllNFT description 4',
+  },
+];
+
+const myNFTData = [
+  {
+      title: 'MyNFT 1',
+      description: 'MyNFT description 1',
+  },
+  {
+      title: 'MyNFT 2',
+      description: 'MyNFT description 2',
+  },
+  {
+      title: 'MyNFT 3',
+      description: 'MyNFT description 3',
+  },
+  {
+      title: 'MyNFT 4',
+      description: 'MyNFT description 4',
+  },
+];
 
 const items = [
   {
     label: 'Minter a new Life NFT',
     key: 'minter',
-    icon: <MailOutlined />,
+    icon: <PlusSquareOutlined />,
   },
   {
     label: 'My NFTs',
@@ -20,18 +59,26 @@ const items = [
     icon: <AppstoreOutlined />,
   },
 ];
+
 const App = () => {
   const [current, setCurrent] = useState('minter');
+  const [NFTData, setNFTData] = useState('');
   const onClick = (e) => {
     console.log('click ', e);
     setCurrent(e.key);
+    if(e.key == "my"){
+      setNFTData(myNFTData);
+    }else if(e.key == "all"){
+      setNFTData(allNFTData);
+    }
+
   };
   return (
     <div>
       <Menu onClick={onClick} selectedKeys={[current]} mode="horizontal" items={items} />
       {current == 'minter' && <Minter />}
-      {current == 'my' && <div>My NFTs</div>}
-      {current == 'all' && <div>All NFTs</div>}
+      {current == 'my' && <NFTList data = { NFTData }/>}
+      {current == 'all' && <NFTList data = { NFTData }/>}
     </div>
   );
 };
---
+++ src/components/NFTList.js
@@ -0,0 +1,29 @@
+import { Card, List } from 'antd';
+
+const { Meta } = Card;
+
+const NFTList = ({ data }) => (
+    <div style={{ display: 'flex', justifyContent: 'center' }}>
+        <List
+            grid={{
+                gutter: 16,
+                column: 4,
+            }}
+            dataSource={data}
+            renderItem={(item) => (
+                // 渲染每个列表项的内容
+                <List.Item>
+                    <Card
+                        hoverable
+                        style={{ width: 360 }}
+                        cover={<img alt="example" src="/images/img1.jpg" style={{ height: 240 }} />}
+                    >
+                        <Meta title={item.title} description={item.description} />
+                    </Card>
+                </List.Item>
+            )}
+        />
+    </div>
+);
+
+export default NFTList;
